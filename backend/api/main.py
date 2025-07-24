21283477a6343755877a3dec1fceace1c318575a
Taliotis <Alyssa.Taliotis@AXT0630-M2507.local>
2025-07-24 14:13:14 -0700

    Initial commit

--- backend/api/main.py
+++ backend/api/main.py
@@ -37,10 +37,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://instalily-lead-gen-frontend.onrender.com/",  # your static frontend URL!
         "http://localhost:3000", 
-        "http://127.0.0.1:3000",
-        "http://localhost:3001",    
-        "http://127.0.0.1:3001"     
+        "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
